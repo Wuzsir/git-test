5e2c9fd3b70f270f98206601295cf05ea140dab3
wzw <wzw@123.com>
2022-02-09 18:31:26 +0800

    master commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master git3");
         System.out.println("hot-fix hello git");
+        System.out.println("hot-fix hello git");
     }
 }
